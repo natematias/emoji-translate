4cc7ed82d3c34a79f90fd38141cd82adc7187355
Nathan Matias <natematias@gmail.com>
2014-12-18 00:15:25 -0500

    added synonym support

--- txt2emoji.py
+++ txt2emoji.py
@@ -3,6 +3,7 @@ import sys
 import json
 import nltk
 from nltk.stem.porter import *
+from nltk.corpus import wordnet
 import random
 
 
@@ -18,18 +19,29 @@ for line in codecs.open(sys.argv[1]):
   emojicount = 0
   for token in tokens:
     stem = stemmer.stem(token)
-    stems.append(stem)
+    #stems.append(stem)
+
+    l = [x.lemmas for x in [x for x in wordnet.synsets(stem)]]
+    synonyms = [x.name for x in [item for sublist in l for item in sublist]]
+
+
     k = emojihash.keys()
     random.shuffle(k)
-    for emo in k:
-      try:
-        if emo.lower().find(stem)>=0:
-          sys.stdout.write(unichr(int(emojihash[emo][u'unified'], 16)).encode('utf-8') + " ")
-          #print " " + stem + " : "  + emo.lower()
-          emojicount += 1
-          break
-      except:
-        continue
+    match = False
+    for word in synonyms:
+      for emo in k:
+        try:
+          if emo.lower().find(word)>=0:
+            sys.stdout.write(unichr(int(emojihash[emo][u'unified'], 16)).encode('utf-8') + " ")
+            #print " " + stem + " : "  + emo.lower()
+            emojicount += 1
+            match = True
+            break
+        except:
+          continue
+      if(match):
+        break
+
   if(emojicount>0):
     sys.stdout.write("\n")
   #print line
